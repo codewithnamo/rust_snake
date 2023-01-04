9861b05629b1cc66b56408e579fa4cf7e809417e
codewithnamo <hannamokim@gmail.com>
2023-01-04 15:40:20 +0900

    print messages with cargo run

---
+++ main.rs
@@ -0,0 +1,17 @@
+
+
+
+fn main() {
+    let message = "Hello World";
+    let message_2 = print_welcome(message);
+    println!("{}", message_2);
+}
+
+fn print_welcome(text: &str) -> &str {
+    println!("{}", text);
+    let new_message = "Hi There";
+    new_message
+}
+
+
+
