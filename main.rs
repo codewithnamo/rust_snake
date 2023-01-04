34d1d96a52fc352fe637e45a84e81391d0151d4d
codewithnamo <hannamokim@gmail.com>
2023-01-04 16:53:53 +0900

    add primitive numbers

--- main.rs
+++ main.rs
@@ -1,13 +1,15 @@
 
 
 fn main() {
-    let is_it_fun = false;
-    let num = -10;
-
-    let small_num:u8 = 255;
-    let small_num_2:i8 = 127; 
-
-
+    let custom_num = 98_000;
+    let hex_num = 0xfa;
+    let bin_num = 0b0010_1011;
+    let byte_num = b'A';
+    
+    println!("{}", custom_num); 
+    println!("{}", hex_num); 
+    println!("{}", bin_num); 
+    println!("{}", byte_num); 
 }
 
 
