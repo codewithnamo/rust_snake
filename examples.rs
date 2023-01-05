f45926912c8514233b6b997b73ee25a2ca0a6c76
codewithnamo <hannamokim@gmail.com>
2023-01-05 14:07:32 +0900

    AI Stack part 2

---
+++ examples.rs
@@ -0,0 +1,34 @@
+fn main () {
+    c();
+    d();
+    f();
+}
+
+fn a(){
+    println!("calling A");
+    e();
+}
+
+fn b(){
+    println!("calling B");
+    
+}
+
+fn c(){
+    println!("calling C");
+    
+}
+
+fn d(){
+    println!("calling A");
+    a();
+}
+
+fn e(){
+    println!("calling E");
+}
+
+fn f(){
+    println!("calling F");
+    b();
+}
