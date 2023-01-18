09f2940358538f673fc2e8b38b0e9524e4cf3820
codewithnamo <hannamokim@gmail.com>
2023-01-18 16:42:11 +0900

    complete 10_modularity001

--- main.rs
+++ main.rs
@@ -1,16 +1,15 @@
 
+use snake_game::Person;
+use snake_game::Animal;
+use snake_game::log_info;
+use snake_game::log_info_2;
 
 fn main() {
-  let message = String::from("Hello");
-  print_message(message);
+  let mut person = Person::new();
+  let animal = Animal(String::from("dog"));
+
+  person.change_age(38);
+
+  log_info(person);
+  log_info_2(&animal);
 }
-
-fn print_message(a: String) {
-    print!("{}", a);
-    let c = a;
-}
-
-
-
-
-
---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+pub trait Log {
+    fn display_info(&self);
+    fn alert_something(&self){
+        println!("Default implementation")
+    }
+}
+
+#[derive(Debug)]
+enum PersonId {
+    Passport(u32),
+    IdentityCard(u32, u32, u32),
+}
+
+pub struct Person {
+    name: String,
+    last_name: String,
+    age: u32,
+    id: PersonId,
+}
+
+pub struct Animal(pub String);
+
+impl Log for Animal {
+    fn display_info(&self) {
+        println!("{}", self.0)
+    }
+
+    fn alert_something(&self) {
+        println!("Animal impelmentation")
+    }
+}
+
+impl Log for Person {
+    fn display_info(&self) {
+        println!("{} {} {} {:?}", self.name, self.last_name, self.age, self.id)
+    }
+}
+
+impl Person {
+    pub fn new() -> Person {
+        Person {
+            name: "Default".to_string(),
+            last_name: "Default".to_string(),
+            age: 0,
+            id: PersonId::IdentityCard(540, 320, 100),
+        }
+    }
+
+    fn from(name: String, last_name: String, age: u32, id: PersonId) -> Person {
+        Person {
+            name,
+            last_name,
+            age, 
+            id
+        }
+    }
+
+    pub fn change_age(&mut self, new_age: u32) {
+        self.age = new_age;
+    }
+}
+
+pub fn log_info(val: impl Log) {
+    val.alert_something();
+}
+
+pub fn log_info_2(val: &dyn Log) {
+    val.alert_something();
+}
