39996c4b1b8b3414a5576b40bb8f229010e8553b
codewithnamo <hannamokim@gmail.com>
2023-01-18 17:40:42 +0900

    complete 10_004nested modules

--- main.rs
+++ main.rs
@@ -1,13 +1,8 @@
 
-use snake_game::learning_rust::{Person, Animal, log_info, log_info_2};
+use snake_game::learning_rust::{Person, Log};
 
 
 fn main() {
-  let mut person = Person::new();
-  let animal = Animal(String::from("dog"));
-
-  person.change_age(38);
-
-  log_info(person);
-  log_info_2(&animal);
+  let person = Person::new();
+  person.display_info();
 }
